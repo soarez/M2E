0fcce4a626ce7844e2a7d38746d7add549a62ad4
Igor Soarez <igorsoarez@gmail.com>
2013-01-30 13:15:13 +0000

    Add concatenation build step

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,43 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    distName: '<%= pkg.name %>-<%= pkg.version %>',
+
     jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', '!test/vendor/**'],
+      src: [
+        'Gruntfile.js',
+        'lib/**/*.js',
+        '!lib/intro.js',
+        '!lib/outro.js',
+        'test/**/*.js',
+        '!test/vendor/**'
+      ],
+      concatenated: 'dist/<%= distName %>.js',
       options: {
         browser: true
+      }
+    },
+
+    concat: {
+      dist: {
+        src: [
+          'lib/intro.js',
+          'lib/event_manager.js',
+          'lib/m2e.js',
+          'lib/outro.js'
+        ],
+        dest: 'dist/<%= distName %>.js'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('lint', 'jshint');
+  grunt.registerTask('prelint', 'jshint:src');
+  grunt.registerTask('cat', 'concat:dist');
+  grunt.registerTask('postlint', 'jshint:concatenated');
+  grunt.registerTask('default', ['prelint', 'cat', 'postlint']);
 
 };
